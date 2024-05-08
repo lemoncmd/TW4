9ac58039a4a224eea66a338c450ffe8062caedb9
lemoncmd <lemoncmd@gmail.com>
2024-05-09 08:00:33 +0900

    reset cpu

--- src/cpu.sv
+++ src/cpu.sv
@@ -8,6 +8,14 @@ module cpu (
     input logic [3:0] in,
     output logic [3:0] out
 );
+  logic [3:0] a, b;
 
+  always_ff @(posedge reset) begin
+    a <= 0;
+    b <= 0;
+    addr.virt_addr.mode <= 0;
+    addr.virt_addr.addr <= 0;
+    out <= 0;
+  end
 endmodule
 
