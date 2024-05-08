18b67a6bc6916e0bd2bc111f7739e0c324b4664a
lemoncmd <lemoncmd@gmail.com>
2024-05-09 08:26:15 +0900

    add instructions

--- src/cpu.sv
+++ src/cpu.sv
@@ -17,5 +17,32 @@ module cpu (
     addr.virt_addr.addr <= 0;
     out <= 0;
   end
+
+  always_ff @(posedge clock) begin
+    logic imm = data.instruction.imm;
+    unique case (data.instruction.opcode)
+      ADD_A_IMM: a <= a + imm;
+      MOV_A_B: a <= b;
+      IN_A: a <= in;
+      MOV_A_IMM: a <= imm;
+
+      MOV_B_A: b <= a;
+      ADD_B_IMM: b <= b + imm;
+      IN_B: b <= in;
+      MOV_B_IMM: b <= imm;
+
+      // NOP0: ;
+      OUT_B:   out <= b;
+      // NOP1: ;
+      OUT_IMM: out <= imm;
+
+      // NOP2: ;
+      // NOP3: ;
+      JMP: addr.virt_addr.addr <= imm;
+      JNC: addr.virt_addr.addr <= imm;
+
+      default: ;
+    endcase
+  end
 endmodule
 
