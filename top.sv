0f45ac1bf6cbc23a9f97ae99e820bb980e7aa22a
lemoncmd <lemoncmd@gmail.com>
2024-05-08 23:47:24 +0900

    reset and output led when changed

--- top.sv
+++ top.sv
@@ -26,6 +26,12 @@ module top;
   );
 
   initial begin
-    $finish;
+    reset = 1;
+    #20 reset = 0;
+    #100 $finish;
+  end
+
+  always_ff @out begin
+    $display("LED: %d", out);
   end
 endmodule
