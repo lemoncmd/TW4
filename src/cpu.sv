d3d7526db81a001673650080ded518e74d712521
lemoncmd <lemoncmd@gmail.com>
2024-05-15 20:40:30 +0900

    move logics to function

--- src/cpu.sv
+++ src/cpu.sv
@@ -8,22 +8,23 @@ module cpu (
     input logic [3:0] in,
     output logic [3:0] out
 );
-  register_t regs;
+  register_t user_regs;
 
-  always_ff @(posedge clock) begin
-    logic [3:0] opcode = data.instruction.opcode;
-    logic [3:0] imm = data.instruction.imm;
+  logic [3:0] opcode = data.instruction.opcode;
+  logic [3:0] imm = data.instruction.imm;
+
+  function static void execute(input logic [3:0] in, ref register_t regs);
     addr.virt_addr.addr <= addr.virt_addr.addr + 1;
     unique case (opcode)
-      ADD_A_IMM: {regs.c, regs.a} <= {1'b0, regs.a} + {1'b0, imm};
-      MOV_A_B: regs.a <= regs.b;
-      IN_A: regs.a <= in;
-      MOV_A_IMM: regs.a <= imm;
+      ADD_A_IMM: {regs.c, regs.a} = {1'b0, regs.a} + {1'b0, imm};
+      MOV_A_B: regs.a = regs.b;
+      IN_A: regs.a = in;
+      MOV_A_IMM: regs.a = imm;
 
-      MOV_B_A: regs.b <= regs.a;
-      ADD_B_IMM: {regs.c, regs.b} <= {1'b0, regs.b} + {1'b0, imm};
-      IN_B: regs.b <= in;
-      MOV_B_IMM: regs.b <= imm;
+      MOV_B_A: regs.b = regs.a;
+      ADD_B_IMM: {regs.c, regs.b} = {1'b0, regs.b} + {1'b0, imm};
+      IN_B: regs.b = in;
+      MOV_B_IMM: regs.b = imm;
 
       // NOP0: ;
       OUT_B:   out <= regs.b;
@@ -37,13 +38,21 @@ module cpu (
 
       default: ;
     endcase
+  endfunction
 
+  function static void reset_regs(ref register_t regs);
+    regs.a = 0;
+    regs.b = 0;
+    regs.c = 0;
+  endfunction
+
+  always_ff @(posedge clock, negedge reset) begin
     if (~reset) begin
-      regs.a <= 0;
-      regs.b <= 0;
-      regs.c <= 0;
+      reset_regs(user_regs);
       addr.virt_addr.addr <= 0;
       out <= 0;
+    end else begin
+      execute(in, user_regs);
     end
 
   end
