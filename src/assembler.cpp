5feec8b412d055dcfe7b1fd45b71a72d906d7e52
lemoncmd <lemoncmd@gmail.com>
2024-05-21 17:16:28 +0900

    add token types

--- src/assembler.cpp
+++ src/assembler.cpp
@@ -3,6 +3,37 @@
 #include <iostream>
 #include <iterator>
 #include <string>
+#include <vector>
+
+enum class TokenKind {
+  section,
+  user,
+  swi,
+  exception,
+  irq,
+  add,
+  mov,
+  in,
+  out,
+  imsk,
+  swap,
+  iret,
+  jnc,
+  jmp,
+  a,
+  b,
+  colon,
+  comma,
+  label,
+  imm
+};
+
+struct Token {
+  TokenKind kind;
+  std::string value;
+};
+
+std::vector<Token> tokenize(std::string s) { return {}; }
 
 int main(int argc, const char *argv[]) {
   if (argc != 2) {
@@ -13,4 +44,6 @@ int main(int argc, const char *argv[]) {
   auto file = std::ifstream(argv[1]);
   auto content = std::string(std::istreambuf_iterator<char>(file),
                              std::istreambuf_iterator<char>());
+
+  auto tokens = tokenize(content);
 }
